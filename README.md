5ec70dcb3ae72e86bf094685320bd3b9dae03bab
Masterain <i@irain.in>
2021-12-23 19:44:00 -0800

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Anime-MKV-Plex-Packager
 A Python script to allow Plex load CN-Subs Anime MKV 
+
+### Feature
+
+- Merge original MKV video with subtitles, subtitle fonts and external audio tracks to meet requirements of Plex server
+  - Simplified-Chinese will be added as ```chi``` language and the tracker will be named as ```chs```
+  - Traditional Chinese will be added as ```chi``` language and the tracker will be named as ```cht``
+  - Subtitle fonts will be added as attachment and Plex will load them automictically
+  - External audio tracks (usually 5.1 Channel) will be added and keep original tracker information
+- (Optional) Rename or remove original files
+  - Rename mode will add ```.bak``` at the end of file name
+- A friendly reminder that the encoding and subtitling groups distribute the multi-channel audio and font files separately to save storage space, and that running this script increases the storage space used by the episodes, a change that is sometimes significant.
+
+### How to  Use
+
+- Install [MKVToolNix](https://mkvtoolnix.download/) and add it to System Path
+
+  - Or, always copy a ```mkvmerge.exe``` with ```main.py``` together
+
+- Copy ```main.py``` to the anime series folder
+
+  - Change the setting in the ```Global Variables``` parts
+
+  - Default settings are:
+
+    ```python
+    # Global Variable
+    DELETE_FONTS = True
+    DELETE_ORIGINAL_MKV = False
+    RENAME_ORIGINAL_MKV = True
+    DELETE_ORIGINAL_MKA = False
+    RENAME_ORIGINAL_MKA = True
+    DELETE_CHS_SUB = False
+    DELETE_CHT_SUB = False
+    ```
+
+- Make sure all associated content has same name with each corresponding episode
+
+  - ```sc.ass``` and ```chs.ass``` are considered as Simplified-Chinese subtitle
+  - ```tc.ass``` and ```cht.ass`` are considered as Transitional-Chinese subtitle
+  - The rules are mostly following VCB-Studio naming rules
+
+- Run ```main.py``` in the Command Line
+
+### Example
+
+- Here's an example in which files will be considered as a group
+
+```powershell
+PS D:\Downloads\[Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p]> python main.py
+Find associated CHS subtitle: [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].chs_v2.ass
+Find associated CHT subtitle: [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].cht_v2.ass
+Find associated audio: [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].mka
+Find main video: [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].mkv
+Running with command:
+"mkvmerge -o [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac]_Plex.mkv --language 0:und --default-track 0:1 --forced-track 0:0 -d 0 -A -S [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].mkv --language 1:jpn --default-track 1:1 --forced-track 1:0 -D -a 1 -S [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].mkv --track-name 0:chs --language 0:chi --default-track 0:1 --forced-track 0:0 -D -A -s 0 [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].chs_v2.ass --track-name 0:cht --language 0:chi --default-track 0:0 --forced-track 0:0 -D -A -s 0 [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].cht_v2.ass --default-track 0:0 --forced-track 0:0 -D -a 0 -S [Mabors-Sub&Kamigami&KTXP&VCB-Studio] Saenai Heroine no Sodatekata Fine [Ma10p_1080p][x265_flac].mka"
+====================
+Task finished. Press any key to exit...
+```
+
+
+
---
+++ main.py
@@ -0,0 +1,109 @@
+import os
+import shutil
+import zipfile
+from pymkv import MKVFile, MKVTrack
+from pathlib import Path
+
+# Global Variable
+DELETE_FONTS = True
+DELETE_ORIGINAL_MKV = False
+RENAME_ORIGINAL_MKV = True
+DELETE_ORIGINAL_MKA = False
+RENAME_ORIGINAL_MKA = True
+DELETE_CHS_SUB = False
+DELETE_CHT_SUB = False
+
+
+# https://gist.github.com/hideaki-t/c42a16189dd5f88a955d
+def unzip(f, encoding):
+    font_list = []
+    with zipfile.ZipFile(f) as z:
+        for i in z.namelist():
+            font_list.append("Fonts/" + i.encode('cp437').decode(encoding))
+            n = Path("Fonts/" + i.encode('cp437').decode(encoding))
+            if i[-1] == '/':
+                if not n.exists():
+                    n.mkdir()
+            else:
+                with n.open('wb') as w:
+                    w.write(z.read(i))
+    return font_list
+
+
+if __name__ == '__main__':
+    if DELETE_ORIGINAL_MKV and RENAME_ORIGINAL_MKV:
+        print("Rename MKV instead")
+        DELETE_ORIGINAL_MKV = False
+    if DELETE_ORIGINAL_MKA and RENAME_ORIGINAL_MKA:
+        print("Rename MKA instead")
+        DELETE_ORIGINAL_MKA = False
+    delete_list = []
+    rename_list = []
+
+    # Prepare fonts
+    folder_list = os.listdir()
+    font_list = []
+    for file_name in folder_list:
+        if "Font" in file_name and ".zip" in file_name:
+            print("Find font package file: " + file_name)
+            if not os.path.exists("Fonts"):
+                os.makedirs("Fonts")
+                print("Fonts sub-directory created")
+            font_list = unzip(file_name, "GBK")
+            print("Unzipped to /Fonts: " + str(font_list))
+            print("=" * 20)
+
+    # Main tasks
+    for file_name in folder_list:
+        if ".mkv" in file_name:
+            episode_name = file_name.replace(".mkv", "")
+            this_task = MKVFile(file_name)
+            for item in folder_list:
+                if episode_name in item:
+                    if ".mkv" in item:
+                        print("Find main video: " + item)
+                        if DELETE_ORIGINAL_MKV:
+                            delete_list.append(item)
+                        if RENAME_ORIGINAL_MKV:
+                            rename_list.append(item)
+                    if ".ass" in item:
+                        if "chs" in item or "sc" in item:
+                            this_chs = MKVTrack(item, track_name="chs", default_track=True, language="chi")
+                            this_task.add_track(this_chs)
+                            print("Find associated CHS subtitle: " + item)
+                            if DELETE_CHS_SUB:
+                                delete_list.append(item)
+                        if "cht" in item or "tc" in item:
+                            this_cht = MKVTrack(item, track_name="cht", default_track=False, language="chi")
+                            this_task.add_track(this_cht)
+                            print("Find associated CHT subtitle: " + item)
+                            if DELETE_CHT_SUB:
+                                delete_list.append(item)
+                    if ".mka" in item:
+                        this_task.add_track(item)
+                        print("Find associated audio: " + item)
+                        if DELETE_ORIGINAL_MKA:
+                            delete_list.append(item)
+                        if RENAME_ORIGINAL_MKA:
+                            rename_list.append(item)
+            for font in font_list:
+                this_task.add_attachment(font)
+            newMKV_name = episode_name + "_Plex.mkv"
+            this_task.mux(newMKV_name)
+            print("=" * 20)
+
+    # Clean up
+    try:
+        shutil.rmtree("Fonts/")
+        print("Remove Fonts Folder Successfully")
+    except:
+        print("Remove Fonts Folder Error")
+    for file in delete_list:
+        try:
+            os.remove(file)
+        except:
+            print("Failed to delete " + file)
+    for file in rename_list:
+        os.rename(file, file + ".bak")
+
+    input("Task finished. Press any key to exit...")
