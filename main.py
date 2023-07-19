73c53aefe60fe8bd655f9176388c337e1151dff5
Masterain <i@irain.in>
2023-07-18 18:21:39 -0700

    l10n structure
    #4

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ from pymkv import MKVFile, MKVTrack
 import py7zr
 import re
 import patoolib
+import gettext
 from config import get_config
 from compressed import unzip
 from subtitle_utils import subtitle_info_checker, is_font_file
 
 # Global Variable
+# i10n
+for language in ["en", "zh-CN"]:
+    language_translations = gettext.translation("main", localedir="locales", languages=[language])
+    language_translations.install()
+_ = language_translations.gettext
 try:
     config = get_config()
 except ValueError:
-    input("Config file error, press ENTER to exit")
+    input(_("Config file error, press ENTER to exit"))
     exit(0)
 DELETE_FONTS = config["TaskSettings"]["DeleteFonts"]
 DELETE_ORIGINAL_MKV = config["TaskSettings"]["DeleteOriginalMKV"]
@@ -26,7 +32,7 @@ if SUFFIX_NAME == "":
 if config["mkvmerge"]["path"] != "":
     MKVMERGE_PATH = config["mkvmerge"]["path"]
 else:
-    print("mkvmerge path not set, using mkvmerge.exe in the working directory")
+    print(_("mkvmerge path not set, using mkvmerge.exe in the working directory"))
     MKVMERGE_PATH = "mkvmerge"
 
 
@@ -34,7 +40,7 @@ def main():
     delete_list = []
     move_list = []
 
-    custom_workdir = input("Please input the working directory (default: current directory): ")
+    custom_workdir = input(_("Please input the working directory (default: current directory): "))
     if custom_workdir != "":
         os.chdir(custom_workdir)
     folder_list = os.listdir()
@@ -49,38 +55,41 @@ def main():
         for file_name in folder_list:
             # if there is a zipped fonts file, unzip it
             if "font" in file_name.lower():
-                print("Find font package file: " + file_name)
+                print(_("Find font package file: ") + file_name)
                 if not os.path.exists("Fonts"):
                     os.makedirs("Fonts")
-                    print("Fonts sub-directory created")
+                    print(_("Fonts sub-directory created"))
                 if ".zip" in file_name:
                     # zip extension
-                    unfiltered_font_list = unzip(file_name, "utf-8")
-                    print("Unzipped to /Fonts: " + str(unfiltered_font_list))
+                    try:
+                        unfiltered_font_list = unzip(file_name, "utf-8")
+                        print(_("Unzipped to /Fonts: ") + str(unfiltered_font_list))
+                    except UnicodeDecodeError:
+                        print(_("Unsupported encoding, please manually zip the file"))
+                        exit(0)
                 elif ".7z" in file_name:
                     # 7z extension
                     with py7zr.SevenZipFile(file_name, mode='r') as z:
                         z.extractall("Fonts")
                         unfiltered_font_list = z.getnames()
-                        print("Unzipped to /Fonts: " + str(unfiltered_font_list))
+                        print(_("Unzipped to /Fonts: ") + str(unfiltered_font_list))
                 elif ".rar" in file_name:
                     if UNRAR_PATH != "":
                         patoolib.extract_archive(file_name, outdir="./Fonts/", program=UNRAR_PATH)
                         unfiltered_font_list = os.listdir("Fonts")
                     else:
-                        print("Unrar path not set, please manually unrar the file")
+                        print(_("Unrar path not set, please manually unrar the file"))
                 else:
-                    print("%s is an unrecognized compressed file format, please manually unzip the file" % file_name)
+                    print(_("%s is an unrecognized compressed file format, please manually unzip the file") % file_name)
                 print("=" * 20)
     # Filter fonts file
     font_list = list(filter(is_font_file, unfiltered_font_list))
-    print("Loading fonts: " + str(font_list))
+    print(_("Loading fonts: ") + str(font_list))
 
     # Generate two useful list to reduce chance traversing folder_list in main task loop
     folder_mkv_list = [file for file in folder_list if file.endswith(".mkv")]
     folder_other_file_list = [file for file in folder_list if not file.endswith(".mkv") and "." in file]
 
-    # input("waiting...")
     # Main tasks
     # Traverse all MKV files (videos) in folder
     for MKV_file_name in folder_mkv_list:
@@ -89,17 +98,16 @@ def main():
 
         if SUFFIX_NAME not in MKV_file_name:
             # Generate the task with MKV file
-            print("Task start: " + MKV_file_name)
+            print(_("Task start: ") + MKV_file_name)
             mkv_name_no_extension = MKV_file_name.replace(".mkv", "")
             this_task = MKVFile(MKV_file_name, mkvmerge_path=MKVMERGE_PATH)
             for track in this_task.tracks:
                 if track.track_type == "audio" or track.track_type == "subtitles":
                     if track.language == "und":
-                        print("Track %s (%s track) language is undefined, set a language for it"
+                        print(_("Track %s (%s track) language is undefined, set a language for it")
                               % (track.track_id, track.track_type))
-                        input("Input a language code for this track"
-                              " (zh-Hans/zh-Hant/jp/en/ru or other language code): ")
-                        track.language_ietf = input()
+                        track.language_ietf = input(_("Input a language code for this track"
+                                                      " (zh-Hans/zh-Hant/jp/en/ru or other language code): "))
 
             if DELETE_ORIGINAL_MKV:
                 delete_list.append(MKV_file_name)
@@ -112,7 +120,7 @@ def main():
                     if item.endswith(".ass"):
                         # Add Subtitle track
                         this_sub_info = subtitle_info_checker(item)
-                        print("Subtitle info: " + str(this_sub_info))
+                        print(_("Subtitle info: ") + str(this_sub_info))
                         if this_sub_info["language"] != "":
                             if config["Subtitle"]["ShowSubtitleAuthorInTrackName"]:
                                 track_name = this_sub_info["language"] + " " + this_sub_info["sub_author"]
@@ -126,7 +134,7 @@ def main():
                                 this_sub_track.default_track = True
                             skip_this_task = False
                             this_task.add_track(this_sub_track)
-                            print("Find " + this_sub_info["language"] + " subtitle: " + item)
+                            print(_("Find ") + this_sub_info["language"] + _(" subtitle: ") + item)
 
                             if DELETE_SUB:
                                 delete_list.append(item)
@@ -136,7 +144,7 @@ def main():
                         # Add MKA track
                         skip_this_task = False
                         this_task.add_track(item)
-                        print("Find associated audio: " + item)
+                        print(_("Find associated audio: ") + item)
                         if DELETE_ORIGINAL_MKA:
                             delete_list.append(item)
                         else:
@@ -161,7 +169,7 @@ def main():
                             # this_ep_num =  01. (one space before and one dot after the number)
                             sub_matched = True
                         if sub_matched:
-                            print("Using ep number " + this_ep_num + " to match subtitle file")
+                            print(_("Using ep number ") + this_ep_num + _(" to match subtitle file"))
                             if item.endswith(".ass"):
                                 this_sub_info = subtitle_info_checker(item)
                                 print(this_sub_info)
@@ -178,13 +186,12 @@ def main():
                                         this_sub_track.default_track = True
                                     skip_this_task = False
                                     this_task.add_track(this_sub_track)
-                                    print("Find " + this_sub_info["language"] + " subtitle: " + item)
+                                    print(_("Find ") + this_sub_info["language"] + _(" subtitle: ") + item)
 
                                     if DELETE_SUB:
                                         delete_list.append(item)
                                     else:
                                         move_list.append(item)
-            # input("waiting...")
             for font in font_list:
                 font = "Fonts/" + font
                 this_task.add_attachment(font)
@@ -193,13 +200,13 @@ def main():
                 try:
                     print("")
                     this_task.mux(new_mkv_name, silent=True)
-                    print("Mux successfully: " + new_mkv_name)
+                    print(_("Mux successfully: ") + new_mkv_name)
                 except subprocess.CalledProcessError:
                     # A mysterious error will not cause any problem
-                    print("MKVMerge raised error: " + new_mkv_name)
+                    print(_("MKVMerge raised error: ") + new_mkv_name)
                 print("=" * 20)
             else:
-                print("No task for this MKV")
+                print(_("No task for this MKV"))
 
     try:
         # Clean up
@@ -209,21 +216,21 @@ def main():
         if DELETE_FONTS:
             try:
                 shutil.rmtree("Fonts/")
-                print("Remove Fonts Folder Successfully")
+                print(_("Remove Fonts Folder Successfully"))
             except OSError:
-                print("Remove Fonts Folder Error")
+                print(_("Remove Fonts Folder Error"))
         for file in delete_list:
             try:
                 os.remove(file)
             except OSError:
-                print("Failed to delete " + file)
+                print(_("Failed to delete ") + file)
         for file in move_list:
             shutil.move(file, "Extra/" + file)
 
-        input("Task finished. Press Enter to exit...")
+        input(_("Task finished. Press Enter to exit..."))
     except Exception as e:
         print(e)
-        input("Error at clean up stage. Press Enter to exit...")
+        input(_("Error at clean up stage. Press Enter to exit"))
 
 
 if __name__ == '__main__':
